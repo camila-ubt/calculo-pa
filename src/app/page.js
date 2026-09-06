d39362f87b793fb5d89fa5c49c952b63724f54f8
Camila S. <cnstos@gmail.com>
2026-09-06 12:42:01 -0300

    feat: ativar controles extras no histórico PA

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,11 @@
 import PainelPA from "@/components/PainelPA";
+import HistoricoPAControles from "@/components/HistoricoPAControles";
 
 export default function Home() {
-  return <PainelPA />;
+  return (
+    <>
+      <PainelPA />
+      <HistoricoPAControles />
+    </>
+  );
 }
